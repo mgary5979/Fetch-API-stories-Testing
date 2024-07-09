634e2035bb8cb66e98245d25a12802015d4839ef
mgary5979 <mgary5979@gmail.com>
2024-07-09 22:28:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+document.getElementById("getStories").addEventListener("click", getStories);
+document.getElementById("getUsers").addEventListener("click", getUsers);
+document.getElementById("getPosts").addEventListener("click", getPosts);
+document.getElementById("addPost").addEventListener("submit", addPost);
+
+function getStories() {
+  fetch("stories.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Stories</h2>';
+      data.forEach(function (story) {
+        output += `
+         <ul class="list-group mb-3">
+           <li class="list-group-item">ID:${story.id}</li>
+           <li class="list-group-item">Title:${story.title}</li>
+           <li class="list-group-item">Desc: ${story.description}</li>
+         </ul>
+       `;
+      });
+      document.getElementById("output").innerHTML = output;
+    });
+}
+
+function getUsers() {
+  fetch("users.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Users</h2>';
+      data.forEach(function (user) {
+        output += `
+         <ul class="list-group mb-3">
+           <li class="list-group-item">ID: ${user.id}</li>
+           <li class="list-group-item">Name: ${user.name}</li>
+           <li class="list-group-item">Email: ${user.email}</li>
+         </ul>
+       `;
+      });
+      document.getElementById("output").innerHTML = output;
+    });
+}
+
+function getPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Posts</h2>';
+      data.forEach(function (post) {
+        output += `
+        <div class="card card-body mb-3">
+         <h3>${post.title}</h3>
+         <p>${post.body}</p>
+        </div>
+       `;
+      });
+      document.getElementById("output").innerHTML = output;
+    });
+}
+
+function addPost(e) {
+  e.preventDefault();
+  let title = document.getElementById("title").value;
+  let body = document.getElementById("body").value;
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ title: title, body: body }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
